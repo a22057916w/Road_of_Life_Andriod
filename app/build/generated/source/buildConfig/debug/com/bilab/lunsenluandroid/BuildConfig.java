d74997082f1f875990d0ed840f8dbdbd7c2f7d31
naitea9527 <a22057916w@gmail.com>
2023-09-18 22:51:09 +0800

    re-build the project

---
+++ app/build/generated/source/buildConfig/debug/com/bilab/lunsenluandroid/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bilab.lunsenluandroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bilab.LunSenLu";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 10004;
+  public static final String VERSION_NAME = "1.0.0";
+}
