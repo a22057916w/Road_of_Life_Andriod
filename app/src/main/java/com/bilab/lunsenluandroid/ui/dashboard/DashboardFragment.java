943b83b8b57c361f035e3a1ec553171ac47f9644
naitea9527 <a22057916w@gmail.com>
2023-09-20 01:52:51 +0800

    test RecycleLayout with text adapter

--- app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
@@ -11,18 +11,44 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bilab.lunsenluandroid.MyAdapter;
 import com.bilab.lunsenluandroid.R;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+
+    private RecyclerView recycler_view;
+    private MyAdapter adapter;
+    private ArrayList<String> mData = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        // 準備資料，塞50個項目到ArrayList裡
+        for(int i = 0; i < 50; i++) {
+            mData.add("項目"+i);
+        }
+
+        // 連結元件
+        recycler_view = (RecyclerView) root.findViewById(R.id.recycler_view);
+        // 設置RecyclerView為列表型態
+        recycler_view.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        // 設置格線
+        recycler_view.addItemDecoration(new DividerItemDecoration(this.getActivity(), DividerItemDecoration.VERTICAL));
+
+        // 將資料交給adapter
+        adapter = new MyAdapter(mData);
+        // 設置adapter給recycler_view
+        recycler_view.setAdapter(adapter);
 
         return root;
     }
