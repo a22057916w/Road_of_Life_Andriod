8e17146cf3971cf1167af5081a12a7c722615a7d
naitea9527 <a22057916w@gmail.com>
2023-09-20 11:58:48 +0800

    cradview in recyclelayout

--- app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
@@ -15,8 +15,10 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bilab.lunsenluandroid.Adapter.CourseAdapter;
 import com.bilab.lunsenluandroid.MyAdapter;
 import com.bilab.lunsenluandroid.R;
+import com.bilab.lunsenluandroid.model.CourseModel;
 
 import java.util.ArrayList;
 
@@ -33,22 +35,46 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        // 準備資料，塞50個項目到ArrayList裡
-        for(int i = 0; i < 50; i++) {
-            mData.add("項目"+i);
-        }
 
-        // 連結元件
-        recycler_view = (RecyclerView) root.findViewById(R.id.recycler_view);
-        // 設置RecyclerView為列表型態
-        recycler_view.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        // 設置格線
-        recycler_view.addItemDecoration(new DividerItemDecoration(this.getActivity(), DividerItemDecoration.VERTICAL));
+        RecyclerView courseRV = root.findViewById(R.id.idRVCourse);
 
-        // 將資料交給adapter
-        adapter = new MyAdapter(mData);
-        // 設置adapter給recycler_view
-        recycler_view.setAdapter(adapter);
+        // Here, we have created new array list and added data to it
+        ArrayList<CourseModel> courseModelArrayList = new ArrayList<CourseModel>();
+        courseModelArrayList.add(new CourseModel("DSA in Java", 4, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("Java Course", 3, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("C++ Course", 4, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("DSA in C++", 4, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("Kotlin for Android", 4, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("Java for Android", 4, R.drawable.baseline_star_24));
+        courseModelArrayList.add(new CourseModel("HTML and CSS", 4, R.drawable.baseline_star_24));
+
+        // we are initializing our adapter class and passing our arraylist to it.
+        CourseAdapter courseAdapter = new CourseAdapter(this.getActivity(), courseModelArrayList);
+
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        courseRV.setLayoutManager(linearLayoutManager);
+        courseRV.setAdapter(courseAdapter);
+
+//        // 準備資料，塞50個項目到ArrayList裡
+//        for(int i = 0; i < 50; i++) {
+//            mData.add("項目"+i);
+//        }
+//
+//        // 連結元件
+//        recycler_view = (RecyclerView) root.findViewById(R.id.recycler_view);
+//        // 設置RecyclerView為列表型態
+//        recycler_view.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+//        // 設置格線
+//        recycler_view.addItemDecoration(new DividerItemDecoration(this.getActivity(), DividerItemDecoration.VERTICAL));
+//
+//        // 將資料交給adapter
+//        adapter = new MyAdapter(mData);
+//        // 設置adapter給recycler_view
+//        recycler_view.setAdapter(adapter);
 
         return root;
     }
