e53aca139991e975cd9fbce8398cbf4ee5f6f6fa
naitea9527 <a22057916w@gmail.com>
2023-09-23 00:28:53 +0800

    revert dashboard UI to 9/19

--- app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
@@ -36,45 +36,6 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        RecyclerView courseRV = root.findViewById(R.id.idRVCourse);
-
-        // Here, we have created new array list and added data to it
-        ArrayList<CourseModel> courseModelArrayList = new ArrayList<CourseModel>();
-        courseModelArrayList.add(new CourseModel("心臟病", "冠狀動脈症候群、心臟衰竭、高血壓性、心臟病、心肌梗塞、風濕性心臟病 ...","40%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("不孕症", "123", "20%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("老人癡呆症", "788", "8%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("膀胱癌", "456", "5%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("口腔癌", "456", "20%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("口將癌", "456", "20%", R.drawable.baseline_heart_broken_24));
-        courseModelArrayList.add(new CourseModel("口腔癌", "5456", "20%", R.drawable.baseline_heart_broken_24));
-
-        // we are initializing our adapter class and passing our arraylist to it.
-        CourseAdapter courseAdapter = new CourseAdapter(this.getActivity(), courseModelArrayList);
-        // below line is for setting a layout manager for our recycler view.
-        // here we are creating vertical list so we will provide orientation as vertical
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getActivity(), LinearLayoutManager.VERTICAL, false);
-
-        // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        courseRV.setLayoutManager(linearLayoutManager);
-        courseRV.setAdapter(courseAdapter);
-
-//        // 準備資料，塞50個項目到ArrayList裡
-//        for(int i = 0; i < 50; i++) {
-//            mData.add("項目"+i);
-//        }
-//
-//        // 連結元件
-//        recycler_view = (RecyclerView) root.findViewById(R.id.recycler_view);
-//        // 設置RecyclerView為列表型態
-//        recycler_view.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-//        // 設置格線
-//        recycler_view.addItemDecoration(new DividerItemDecoration(this.getActivity(), DividerItemDecoration.VERTICAL));
-//
-//        // 將資料交給adapter
-//        adapter = new MyAdapter(mData);
-//        // 設置adapter給recycler_view
-//        recycler_view.setAdapter(adapter);
-
         return root;
     }
 }
