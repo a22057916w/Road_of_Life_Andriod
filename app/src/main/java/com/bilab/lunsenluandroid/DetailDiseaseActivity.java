9825f1a45edb08c1328a157606c539104180f802
naitea9527 <a22057916w@gmail.com>
2023-10-04 02:20:37 +0800

    加入疾病至recycleview

--- app/src/main/java/com/bilab/lunsenluandroid/DetailDiseaseActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/DetailDiseaseActivity.java
@@ -1,8 +1,15 @@
 package com.bilab.lunsenluandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.bilab.lunsenluandroid.Adapter.RecycleViewDetailDisease;
+import com.bilab.lunsenluandroid.model.DetailDiseaseModel;
+
+import java.util.ArrayList;
 
 public class DetailDiseaseActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class DetailDiseaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_diease);
+        getSupportActionBar().hide();
+
+        RecyclerView detailDiseaseRV = findViewById(R.id.RVDetailDisease);
+
+        ArrayList<DetailDiseaseModel> detailDiseaseModelArrayList = new ArrayList<DetailDiseaseModel>();
+        detailDiseaseModelArrayList.add(new DetailDiseaseModel("月經異常出血", R.drawable.pregnant));
+
+        RecycleViewDetailDisease recycleViewDetailDisease = new RecycleViewDetailDisease(this, detailDiseaseModelArrayList);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        detailDiseaseRV.setLayoutManager(linearLayoutManager);
+        detailDiseaseRV.setAdapter((recycleViewDetailDisease));
+
     }
+
 }
