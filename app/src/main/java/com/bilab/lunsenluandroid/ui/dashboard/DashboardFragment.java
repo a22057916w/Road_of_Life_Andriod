e9f990cfe725336e100d757f690cdb98c7465f0c
naitea9527 <a22057916w@gmail.com>
2023-10-04 02:21:19 +0800

    子宮癌TV設定為可點擊元件

--- app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/bilab/lunsenluandroid/ui/dashboard/DashboardFragment.java
@@ -1,16 +1,19 @@
 package com.bilab.lunsenluandroid.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bilab.lunsenluandroid.DetailDiseaseActivity;
 import com.bilab.lunsenluandroid.R;
 
 public class DashboardFragment extends Fragment {
@@ -19,6 +22,8 @@ public class DashboardFragment extends Fragment {
 
     private RecyclerView recycler_view;
     private SearchView searchView;
+
+    private TextView uterus_tv;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +34,15 @@ public class DashboardFragment extends Fragment {
         searchView = root.findViewById(R.id.searchView);
         // set default text
         searchView.setQuery("請輸入病症或器官", false);
+
+        uterus_tv = root.findViewById(R.id.TVUterus);
+        uterus_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DetailDiseaseActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 }
