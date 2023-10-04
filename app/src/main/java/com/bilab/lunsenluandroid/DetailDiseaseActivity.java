4df06d739e9398997e92d49eba183839ac8ba72c
naitea9527 <a22057916w@gmail.com>
2023-10-04 13:52:03 +0800

    新增子宮, 卵巢, 膀胱疾病

--- app/src/main/java/com/bilab/lunsenluandroid/DetailDiseaseActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/DetailDiseaseActivity.java
@@ -22,7 +22,48 @@ public class DetailDiseaseActivity extends AppCompatActivity {
         RecyclerView detailDiseaseRV = findViewById(R.id.RVDetailDisease);
 
         ArrayList<DetailDiseaseModel> detailDiseaseModelArrayList = new ArrayList<DetailDiseaseModel>();
-        detailDiseaseModelArrayList.add(new DetailDiseaseModel("月經異常出血", R.drawable.pregnant));
+        detailDiseaseModelArrayList.add(new DetailDiseaseModel("胃腸出血", R.drawable.bladder));
+
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("慢性腎衰竭", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎水腫", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("慢性腎絲球腎炎", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎及輸尿管結石", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎炎，腎病綜合徵和腎病", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("膀胱炎", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他膀胱疾患", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿道感染", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿生殖器官惡性腫瘤", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("侵及泌尿系統之徵候", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他泌尿系統疾病", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("攝護腺（前列腺）增生", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("攝護腺（前列腺）炎性疾病", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("停經及停經後之疾患", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他女生殖器官疾病", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("痔瘡", R.drawable.bladder));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("骨質疏鬆症", R.drawable.bladder));
+
+
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("卵巢良性腫瘤", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("卵巢、輸卵管及闊韌帶之非炎性疾患", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮內膜異位症", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他良性腫瘤", R.drawable.pregnant));
+
+
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他子宮疾患", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮異常出血", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮內膜異位症", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮肌瘤", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他子宮良性腫瘤", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他良性腫瘤", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿生殖器官惡性腫瘤", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("女性生殖器的其他疾病", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("停經及停經後之疾患", R.drawable.pregnant));
+//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("貧血", R.drawable.pregnant));
+
+
+
+
+
 
         RecycleViewDetailDisease recycleViewDetailDisease = new RecycleViewDetailDisease(this, detailDiseaseModelArrayList);
 
