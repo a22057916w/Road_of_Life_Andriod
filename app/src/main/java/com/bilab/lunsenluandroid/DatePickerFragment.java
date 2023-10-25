1a695584f8dd8321213dbe17bff1e2a796ca64e4
naitea9527 <a22057916w@gmail.com>
2023-10-26 02:54:47 +0800

    modify reisiterActivitu layout

--- app/src/main/java/com/bilab/lunsenluandroid/DatePickerFragment.java
+++ app/src/main/java/com/bilab/lunsenluandroid/DatePickerFragment.java
@@ -10,46 +10,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DatePickerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private String date = "";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public DatePickerFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DatePickerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DatePickerFragment newInstance(String param1, String param2) {
-        DatePickerFragment fragment = new DatePickerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public DatePickerDialog onCreateDialog(Bundle savedInstanceState) {
@@ -62,24 +33,20 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         // Create a new instance of DatePickerDialog and return it.
         return new DatePickerDialog(requireContext(), this, year, month, day);
     }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_date_picker, container, false);
-//    }
+
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
+        setDate(year, month, day);
+        TextView birthday_tv = getActivity().findViewById(R.id.birthday_textview);
+        birthday_tv.setText((getDate()));
+    }
 
+    public String getDate() {
+        return date;
+    }
+    private void setDate(int year, int month, int day) {
+        date = String.valueOf(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 }
--- app/src/main/java/com/bilab/lunsenluandroid/RegisterSecondStepActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/RegisterSecondStepActivity.java
@@ -3,7 +3,6 @@ package com.bilab.lunsenluandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +12,9 @@ import android.widget.TextView;
 
 public class RegisterSecondStepActivity extends AppCompatActivity {
     EditText name,phone,email,password_check;
-    Button logout_button ,birthday_ev;
+    Button logout_button , birthday_btn;
     ImageView previous;
-    TextView register_error;
+    TextView register_error, birthday_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,26 +24,24 @@ public class RegisterSecondStepActivity extends AppCompatActivity {
         name =  (EditText) findViewById(R.id.logout_name);
         phone =  (EditText) findViewById(R.id.logout_phone);
         email =  (EditText) findViewById(R.id.logout_email);
-        birthday_ev =  (Button) findViewById(R.id.logout_birthday);
-        password_check =  (EditText) findViewById(R.id.logout_password_check);
+
+        birthday_btn =  (Button) findViewById(R.id.logout_birthday);
+        birthday_tv = (TextView) findViewById(R.id.birthday_textview);
+
+//        password_check =  (EditText) findViewById(R.id.logout_password_check);
         logout_button =  (Button) findViewById(R.id.logout_button);
         previous =  (ImageView) findViewById(R.id.register_previous);
         register_error = (TextView) findViewById(R.id.register_error);
 
 
-        birthday_ev.setOnClickListener(new View.OnClickListener() {
+        birthday_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatePickerFragment newFragment = new DatePickerFragment();
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
                 FragmentManager supportFragmentManager = getSupportFragmentManager();
-                newFragment.show(supportFragmentManager, "datePicker");
+                datePickerFragment.show(supportFragmentManager, "datePicker");
             }
         });
-
-//        birthday_ev.setOnClickListener {
-//            DatePickerFragment newFragment = new DatePickerFragment();
-//            newFragment.show(supportFragmentManager, "datePicker");
-//        }
 
     }
 
