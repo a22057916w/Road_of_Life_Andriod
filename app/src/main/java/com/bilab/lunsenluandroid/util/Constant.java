ba0bd6d34b7ae0ad0313d586940be0937af37f2e
naitea9527 <a22057916w@gmail.com>
2023-11-20 23:38:40 +0800

    add EXTRA_DISEASE_CATEGORY

--- app/src/main/java/com/bilab/lunsenluandroid/util/Constant.java
+++ app/src/main/java/com/bilab/lunsenluandroid/util/Constant.java
@@ -2,4 +2,5 @@ package com.bilab.lunsenluandroid.util;
 
 public class Constant {
     public static final String EXTRA_STARTER_ACTIVITY_NAME = "EXTRA_STATER_ACTIVITY_NAME";
+    public static final String EXTRA_DISEASE_CATEGORY = "EXTRA_DISEASE_CATEGORY";
 }
