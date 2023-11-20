5f8ad062d68edeb1ee304947531e610e81137d34
naitea9527 <a22057916w@gmail.com>
2023-11-21 01:00:33 +0800

    set UI according to disease category

--- app/src/main/java/com/bilab/lunsenluandroid/DiseaseSelectionActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/DiseaseSelectionActivity.java
@@ -4,117 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.bilab.lunsenluandroid.Adapter.RecycleViewDiseaseSelection;
 import com.bilab.lunsenluandroid.model.DiseaseSelectionModel;
+import com.bilab.lunsenluandroid.util.Constant;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class DiseaseSelectionActivity extends AppCompatActivity {
 
-    RecyclerView rv_disease_selection;
-    TextView tv_disease_related_disease;
+    private RecyclerView rv_disease_selection;
+    private TextView tv_disease_related_disease;
+    private Intent receiverIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_selection);
+
+        receiverIntent = getIntent();
+        if (receiverIntent == null || receiverIntent.getComponent() == null) {
+            Log.d("DiseaseSelectionActivity", "Do not receive Intent.");
+            throw new NullPointerException();
+        }
 
         registerUI();
         setupUI();
 
-        ArrayList<DiseaseSelectionModel> diseaseSelectionModelArrayList = new ArrayList<DiseaseSelectionModel>();
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("胃腸出血", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("痔瘡", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("功能性消化不良症", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("十二指腸潰瘍", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("消化性潰瘍", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("功能性胃障礙", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他腹部及骨盆疾患", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他腸道和腹膜疾病", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("靜脈和淋巴系疾病及其他循環系統疾病", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他消化系統疾患", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("食道，胃和十二指腸疾病", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他腸道疾病", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他消化系統良性腫瘤", R.drawable.rectum));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他良性腫瘤", R.drawable.rectum));
-
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿道感染", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎及輸尿管結石", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("膀胱炎", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("攝護腺（前列腺）增生", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("慢性腎衰竭", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他排尿系統疾患", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("停經及停經後之疾患", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿生殖器官惡性腫瘤", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他泌尿系統疾病", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他男性生殖器官疾病", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎炎，腎病綜合徵和腎病", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("泌尿生殖器官惡性腫瘤", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("腎水腫", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("慢性腎絲球腎炎", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他膀胱疾患", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("痔瘡", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("骨質疏鬆", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他女性生殖器官疾病", R.drawable.bladder));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("攝護腺（前列腺）炎性疾病", R.drawable.bladder));
-
-
-
-
-
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("卵巢良性腫瘤", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("卵巢、輸卵管及闊韌帶之非炎性疾患", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮內膜異位症", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("子宮肌瘤", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他腹部及骨盆疾患", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("卵巢、輸卵管、骨盆蜂窩組織及腹膜之炎症", R.drawable.pregnant));
-//        detailDiseaseModelArrayList.add(new DetailDiseaseModel("其他良性腫瘤", R.drawable.pregnant));
-
-
-
-
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("其他子宮疾患", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("月經失調及生殖道異常出血", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("子宮肌瘤", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("其他子宮良性腫瘤", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("子宮內膜異位症", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("停經及停經後之疾患", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("貧血", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("泌尿生殖器官惡性腫瘤", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("女性生殖器的其他疾病", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("其他良性腫瘤", R.drawable.pregnant));
-        diseaseSelectionModelArrayList.add(new DiseaseSelectionModel("血液和造血器官疾病", R.drawable.pregnant));
-
-
-
-
-
-
-        RecycleViewDiseaseSelection recycleViewDiseaseSelection = new RecycleViewDiseaseSelection(this, diseaseSelectionModelArrayList);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-
-        rv_disease_selection.setLayoutManager(linearLayoutManager);
-        rv_disease_selection.setAdapter((recycleViewDiseaseSelection));
-
     }
 
-
-
     private void registerUI() {
-        setContentView(R.layout.activity_disease_selection);
-
         rv_disease_selection = findViewById(R.id.rv_disease_selection);
         tv_disease_related_disease = findViewById(R.id.tv_cancer_related_disease);
     }
 
     private void setupUI() {
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         setupRV();
     }
 
     private void setupRV() {
+        ArrayList<String> related_disease = getRelatedDisease();
+        related_disease.add(getString(R.string.none_of_above_disease));
+
+        ArrayList<DiseaseSelectionModel> diseaseSelectionModelArrayList = new ArrayList<DiseaseSelectionModel>();
+
+        for(int i = 0; i < related_disease.size(); i++)
+            diseaseSelectionModelArrayList.add(new DiseaseSelectionModel(related_disease.get(i), R.drawable.ic_uterus));
+
+        RecycleViewDiseaseSelection recycleViewDiseaseSelection = new RecycleViewDiseaseSelection(this, diseaseSelectionModelArrayList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rv_disease_selection.setLayoutManager(linearLayoutManager);
+        rv_disease_selection.setAdapter((recycleViewDiseaseSelection));
+    }
+
+    private ArrayList<String> getRelatedDisease() {
+        String [] uterus_related_disease = {"子宮相關疾病", "月經失調或女性生殖道異常出血", "子宮平滑肌瘤或其他良性腫瘤", "子宮內膜異位症", "貧血症狀"};
+        String [] ovary_related_disease = {"卵巢良性腫瘤", "卵巢或輸卵管非發炎性疾病", "子宮內膜異位症", "子宮平滑肌瘤或其他良性腫瘤", "骨盆腔發炎（子宮、卵巢、輸卵管"};
+        String [] bladder_related_disease = {"泌尿道系統相關疾病", "腎結石或輸尿管結石", "膀胱發炎或相關疾病", "攝護腺（前列腺）肥大或相關疾病", "慢性腎衰竭", "腎絲球腎炎", "腎水腫"};
+        String [] rectum_related_disease = {"腸和腹膜疾病、胃腸出血", "痔瘡", "胃或十二指腸等消化道潰瘍或功能性障礙", "消化系統良性腫瘤"};
+
+        String disease_category = receiverIntent.getStringExtra(Constant.EXTRA_DISEASE_CATEGORY);
+        if(disease_category == null) {
+            Log.d("DiseaseSelectionActivity", "disease_category is NULL.");
+            throw new NullPointerException();
+        }
+
+        if(disease_category.equals(Constant.UTERUS))
+            return new ArrayList<>(List.of(uterus_related_disease));
+        if(disease_category.equals(Constant.OVARY))
+            return new ArrayList<>(List.of(ovary_related_disease));
+        if(disease_category.equals(Constant.BLADDER))
+            return new ArrayList<>(List.of(bladder_related_disease));
+        if(disease_category.equals(Constant.RECTUM))
+            return new ArrayList<>(List.of(rectum_related_disease));
+
+        return new ArrayList<>();
     }
 }
