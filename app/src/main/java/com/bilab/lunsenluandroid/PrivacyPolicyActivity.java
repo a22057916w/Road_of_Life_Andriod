f45a11942f6334ecdf729727b6d861739a60b4d0
naitea9527 <a22057916w@gmail.com>
2023-11-21 23:46:56 +0800

    privacy_policy.xml -> avtivity_privacy_policy.xml

--- app/src/main/java/com/bilab/lunsenluandroid/PrivacyPolicyActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/PrivacyPolicyActivity.java
@@ -2,7 +2,6 @@ package com.bilab.lunsenluandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,7 +18,7 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.privacy_policy);
+        setContentView(R.layout.avtivity_privacy_policy);
         getSupportActionBar().hide();
 
         registerUI();
