a22dfe5f1937ea86a308d7d42ee809dc3d096864
naitea9527 <a22057916w@gmail.com>
2023-12-11 03:44:16 +0800

    Update DiseaseModel.java

--- app/src/main/java/com/bilab/lunsenluandroid/model/DiseaseModel.java
+++ app/src/main/java/com/bilab/lunsenluandroid/model/DiseaseModel.java
@@ -2,7 +2,7 @@ package com.bilab.lunsenluandroid.model;
 
 public class DiseaseModel {
     protected String disease_name;
-    protected  int disease_image;
+    protected int disease_image;
 
     public DiseaseModel(String disease_name, int disease_image) {
         this.disease_name = disease_name;
