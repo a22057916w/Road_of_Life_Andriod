6fdad690999cf01728b0ec7d938bedebca2a6927
naitea9527 <a22057916w@gmail.com>
2023-12-13 03:24:01 +0800

    new class Disease, specify type, name

---
+++ app/src/main/java/com/bilab/lunsenluandroid/Disease.java
@@ -0,0 +1,18 @@
+package com.bilab.lunsenluandroid;
+
+public class Disease {
+    private final String type;
+    private final String name;
+
+    public Disease(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+}
