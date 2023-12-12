60ec134dc3b1f7d55236540b50bf7a2000a1ae26
naitea9527 <a22057916w@gmail.com>
2023-12-13 03:23:32 +0800

    Person singleton to hold Disease, ...

---
+++ app/src/main/java/com/bilab/lunsenluandroid/Person.java
@@ -0,0 +1,52 @@
+package com.bilab.lunsenluandroid;
+
+import com.bilab.lunsenluandroid.util.Constant;
+
+import java.util.ArrayList;
+
+public class Person {
+    private static Person instance;
+
+    private String name;
+    private String weight;
+    private int year;
+
+    private ArrayList<Disease> _diseases;
+    public static synchronized Person getInstance() {
+        if(instance == null)
+            instance = new Person();
+        return instance;
+    }
+
+    public Person() {
+        _diseases = new ArrayList<>();
+    };
+
+    public void addDisease(Disease disease) {
+        if(findDisease(disease) != 0)
+            return;
+        _diseases.add(disease);
+    }
+
+    public void updateDisease(Disease disease) {
+        int pos = findDisease(disease);
+        if(pos != 0)
+            _diseases.remove(pos);
+        else
+            _diseases.add(disease);
+    }
+
+    public int findDisease(Disease disease) {
+        if(_diseases.size() < 1)
+            return Constant.npos;
+
+        int pos = -1;
+        for(Disease dis : _diseases) {
+            pos++;
+            if(dis.getName().equals(disease.getName()) && dis.getType().equals(disease.getType()))
+                break;
+        }
+
+        return pos;
+    }
+}
