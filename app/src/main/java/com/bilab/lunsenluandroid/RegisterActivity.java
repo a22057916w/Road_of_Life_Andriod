bddcb0cf0a9aa8f2af411244f975a7d791703715
naitea9527 <a22057916w@gmail.com>
2023-12-14 04:19:04 +0800

    imp UIs

--- app/src/main/java/com/bilab/lunsenluandroid/RegisterActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/RegisterActivity.java
@@ -3,10 +3,12 @@ package com.bilab.lunsenluandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,34 +17,41 @@ import androidx.fragment.app.FragmentManager;
 import com.bilab.lunsenluandroid.util.Constant;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    EditText email,password_check;
-    Button btn_next, birthday_btn, name_btn, weight_btn;
+    Button btn_next, btn_year, btn_height, btn_weight;
+    RadioButton rb_male, rb_female;
     ImageView previous;
-    TextView register_error, birthday_tv;
+    TextView register_error, tv_gender, tv_height, tv_weight, tv_year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerUI();
+        setupUI();
+    }
+
+    private void registerUI() {
+        btn_height = findViewById(R.id.btn_height);
+        btn_weight = findViewById(R.id.btn_weight);
+        btn_year = findViewById(R.id.btn_year);
+        btn_next = findViewById(R.id.btn_next);
+
+        rb_male = findViewById(R.id.rb_male);
+        rb_female = findViewById(R.id.rb_female);
+
+        tv_height = findViewById(R.id.tv_height);
+        tv_weight = findViewById(R.id.tv_weight);
+        tv_gender = findViewById(R.id.tv_gender);
+        tv_year = findViewById(R.id.tv_year);
+
+        previous =   findViewById(R.id.register_previous);
+        register_error =  findViewById(R.id.register_error);
+    }
+    private void setupUI() {
+        Person person = Person.getInstance();
+
         getSupportActionBar().hide();
-
-        name_btn =  (Button) findViewById(R.id.logout_name);
-
-
-        weight_btn =  (Button) findViewById(R.id.logout_phone);
-
-        email =  (EditText) findViewById(R.id.logout_email);
-
-        birthday_btn =  (Button) findViewById(R.id.btn_year);
-        birthday_tv = (TextView) findViewById(R.id.tv_year);
-
-//        password_check =  (EditText) findViewById(R.id.logout_password_check);
-        btn_next =  (Button) findViewById(R.id.btn_next);
-        previous =  (ImageView) findViewById(R.id.register_previous);
-        register_error = (TextView) findViewById(R.id.register_error);
-
-
-        birthday_btn.setOnClickListener(new View.OnClickListener() {
+        btn_year.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 YearPickerFragment yearPickerFragment = new YearPickerFragment();
@@ -52,7 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // Add listeners for the work for/break for buttons
-        name_btn.setOnClickListener(new View.OnClickListener() {
+        btn_height.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 HeightPickerFragment newFragment = new HeightPickerFragment();
@@ -63,7 +72,7 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        weight_btn.setOnClickListener(new View.OnClickListener() {
+        btn_weight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 WeightPickerFragment newFragment = new WeightPickerFragment();
@@ -82,5 +91,24 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(openRegisterDiseaseIntent);
             }
         });
+
+        rb_male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Person.getInstance().setGender(Constant.MALE);
+                rb_female.setChecked(false);
+                Log.d("88888",Person.getInstance().getGender());
+            }
+        });
+
+        rb_female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Person.getInstance().setGender(Constant.FEMALE);
+                rb_male.setChecked(false);
+                Log.d("88888",Person.getInstance().getGender());
+            }
+        });
     }
+
 }
