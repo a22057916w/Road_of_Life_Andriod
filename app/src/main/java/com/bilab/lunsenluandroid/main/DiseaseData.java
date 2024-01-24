eb5b5d961158945d372fafe811c7b9ba01c28d58
naitea9527 <a22057916w@gmail.com>
2024-01-25 01:45:10 +0800

    Update DiseaseData.java

--- app/src/main/java/com/bilab/lunsenluandroid/main/DiseaseData.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/DiseaseData.java
@@ -2,6 +2,9 @@ package com.bilab.lunsenluandroid.main;
 
 import com.bilab.lunsenluandroid.conf.Constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiseaseData {
     private static DiseaseData _instance;
     private final String [] _uterus_diseases = {"子宮相關疾病", "月經失調或女性生殖道異常出血", "子宮平滑肌瘤或其他良性腫瘤", "子宮內膜異位症", "貧血症狀", "無上述症狀"};
@@ -19,47 +22,15 @@ public class DiseaseData {
     public DiseaseData() {
     }
 
-    public String [] getCancerDiseaseList(String cancer) {
+    public ArrayList<String> getCancerDiseaseList(String cancer) {
         if(cancer.equals(Constant.UTERUS))
-            return _uterus_diseases;
+            return new ArrayList<>(List.of(_uterus_diseases));
         if(cancer.equals(Constant.OVARY))
-            return _ovary_diseases;
+            return new ArrayList<>(List.of(_ovary_diseases));
         if(cancer.equals(Constant.BLADDER))
-            return _bladder_diseases;
+            return new ArrayList<>(List.of(_bladder_diseases));
         if(cancer.equals(Constant.RECTUM))
-            return _rectum_diseases;
+            return new ArrayList<>(List.of(_rectum_diseases));
         return null;
     }
-//    public Map<String, Boolean> getCancerDiseases(String cancer) {
-//        if(cancer.equals(Constant.UTERUS))
-//            return _uterus_map;
-//        if(cancer.equals(Constant.OVARY))
-//            return _ovary_map;
-//        if(cancer.equals(Constant.BLADDER))
-//            return _bladder_map;
-//        if(cancer.equals(Constant.RECTUM))
-//            return _rectum_map;
-//        return null;
-//    }
-//
-//    public void update(String cancer, Map<String, Boolean> map) {
-//        if(cancer.equals(Constant.UTERUS)) {
-//            _uterus_map.clear();
-//            _uterus_map.putAll(map);
-//            for(var entry : _uterus_map.entrySet())
-//                Log.d("3333", entry.getKey() + ": " + entry.getValue());
-//        }
-//        if(cancer.equals(Constant.OVARY))
-//            _ovary_map = map;
-//        if(cancer.equals(Constant.BLADDER))
-//            _bladder_map = map;
-//        if(cancer.equals(Constant.RECTUM))
-//            _rectum_map = map;
-//        throw new RuntimeException();
-//    }
-//
-//    public void updateUterus(String disease) {
-//        boolean check = _uterus_map.get(disease);
-//        _uterus_map.replace(disease, !check);
-//    }
 }
