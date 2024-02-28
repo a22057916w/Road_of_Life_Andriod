7935364775fd053a38fdb6dbfaac3c0c4488d2cd
naitea9527 <a22057916w@gmail.com>
2024-02-29 00:22:28 +0800

    new Gene Names

--- app/src/main/java/com/bilab/lunsenluandroid/main/setting/GeneRegisterActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/GeneRegisterActivity.java
@@ -6,27 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.bilab.lunsenluandroid.R;
+import com.bilab.lunsenluandroid.conf.Constant;
 
 import java.util.Objects;
 
 public class GeneRegisterActivity extends AppCompatActivity {
     Button btn_next, btn_skip;
+    EditText edt_OTX1, edt_GLAR1, edt_ZIC4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gene_register);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        btn_next = findViewById(R.id.btn_next);
-        btn_skip = findViewById(R.id.btn_skip);
-
-
+        registerUI();
+        
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GeneRegisterActivity.this, DNAmChartActivity.class);
+                // put edt values
+                intent.putExtra(Constant.OTX1, Double.parseDouble(edt_OTX1.getText().toString()));
+                intent.putExtra(Constant.GLAR1, Double.parseDouble(edt_GLAR1.getText().toString()));
+                intent.putExtra(Constant.ZIC4, Double.parseDouble(edt_ZIC4.getText().toString()));
+
                 startActivity(intent);
             }
         });
@@ -38,5 +45,14 @@ public class GeneRegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void registerUI() {
+        btn_next = findViewById(R.id.btn_next);
+        btn_skip = findViewById(R.id.btn_skip);
+
+        edt_OTX1 = findViewById(R.id.edt_OTX1);
+        edt_GLAR1 = findViewById(R.id.edt_GLAR1);
+        edt_ZIC4 = findViewById(R.id.edt_ZIC4);
     }
 }
