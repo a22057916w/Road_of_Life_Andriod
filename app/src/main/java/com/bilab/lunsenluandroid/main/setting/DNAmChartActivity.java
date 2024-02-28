9bcf8ef4ae9b19c1c9209626cd7770c769130170
naitea9527 <a22057916w@gmail.com>
2024-02-28 23:59:18 +0800

    add functions to draw boxes and set markers

--- app/src/main/java/com/bilab/lunsenluandroid/main/setting/DNAmChartActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/DNAmChartActivity.java
@@ -10,8 +10,11 @@ import android.widget.Toast;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Cartesian;
 import com.anychart.core.cartesian.series.Box;
+import com.anychart.core.cartesian.series.Marker;
+import com.anychart.enums.MarkerType;
 import com.bilab.lunsenluandroid.R;
 
 
@@ -34,151 +37,25 @@ public class DNAmChartActivity extends AppCompatActivity {
     private ArrayList<ArrayList<Double>> _outliers;
 
     // ============== box properties ================
-    private String[] _name = {"OTX1", "GLAR1", "ZIC4"};
-    private String[] _colors = {"#70CCE1", "#00FF00", "#FFE200"};  // blue, green, yellow
-    private double[] _xPos = {0.35, 0.5, 0.65};
+    private ArrayList<String> _names, _colors;
+    private ArrayList<Double> _xPos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dnam_chart);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        loadConfig();   // load config or default values
+        loadConfig();   // load config for data members
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
 
         Cartesian boxChart = AnyChart.box();
-
-        draw(boxChart);
-
         boxChart.xAxis(0).staggerMode(true);
 
-        // OTX1_N
-//        List<DataEntry> data = new ArrayList<>();
-//        data.add(new CustomBoxDataEntry("正常", _lows.get(0), _q1.get(0), _q2.get(0), _q3.get(0), _highs.get(0), _outliers.get(0).toArray(new Double[0])));
-//
-//        Box box = boxChart.box(data);
-//        box.whiskerWidth("20%");
-//        box.name("OTX1");
-//        box.xPointPosition(0.35);
-//
-//        // GALR1_N
-//        List<DataEntry> data2 = new ArrayList<>();
-//        data2.add(new CustomBoxDataEntry("正常", 0.0720, 0.1117, 0.2152, 0.5517, 0.6560, new Double[]{}));
-//
-//        Box box2 = boxChart.box(data2);
-//        box2.fill("#00FF00");
-//        box2.stroke("#00FF00");
-//        box2.whiskerWidth("20%");
-//        box2.name("GLAR1");
-//        box2.xPointPosition(0.5);
-//
-//        // ZIC4_N
-//        List<DataEntry> data3 = new ArrayList<>();
-//        data3.add(new CustomBoxDataEntry("正常", 0.1020, 0.1253, 0.1870, 0.5095, 0.5918, new Double[]{}));
-//
-//        Box box3 = boxChart.box(data3);
-//        box3.fill("#FFE200");
-//        box3.stroke("#FFE200");
-//        box3.whiskerWidth("20%");
-//        box3.name("ZIC4");
-//        box3.xPointPosition(0.65);
-
-
-        // OTX1_T
-        List<DataEntry> data4 = new ArrayList<>();
-        data4.add(new CustomBoxDataEntry("確診", 0.2955, 0.5970, 0.7005, 0.7980, 0.9514, new Double[]{0.0592, 0.0970, 0.1990, 0.2287}));
-
-        Box box4 = boxChart.box(data4);
-        box4.fill("#70CCE1");
-        box4.stroke("#70CCE1");
-        box4.whiskerWidth("20%");
-        box4.medianStroke("#000000");
-        box4.name("OTX1");
-        box4.legendItem().enabled(false);
-        box4.xPointPosition(0.35);
-
-        // GLAR1_T
-        List<DataEntry> data5 = new ArrayList<>();
-        data5.add(new CustomBoxDataEntry("確診", 0.3349, 0.5964, 0.7161, 0.7322, 0.9463, new Double[]{0.0593, 0.0789, 0.0899, 0.1450, 0.1891, 0.2469}));
-
-        Box box5 = boxChart.box(data5);
-        box5.fill("#00FF00");
-        box5.stroke("#00FF00");
-        box5.whiskerWidth("20%");
-        box5.name("GLAR1");
-        box5.legendItem().enabled(false);
-        box5.xPointPosition(0.5);
-
-
-        List<DataEntry> data6 = new ArrayList<>();
-        data6.add(new CustomBoxDataEntry("確診", 0.1365, 0.4792, 0.6705, 0.7748, 0.9691, new Double[]{}));
-
-        // ZIC4_T
-        Box box6 = boxChart.box(data6);
-        box6.fill("#FFE200");
-        box6.stroke("#FFE200");
-        box6.whiskerWidth("20%");
-        box6.medianStroke("#000000");
-        box6.name("ZIC4");
-        box6.legendItem().enabled(false);
-        box6.xPointPosition(0.65);
-
-//        List<DataEntry> marker1 = new ArrayList<>();
-//        marker1.add(new ValueDataEntry("正常", 0.5));
-//
-//        Marker marker_n1 = boxChart.marker(marker1);
-//        marker_n1.xPointPosition(0.35);
-//        marker_n1.legendItem().enabled(false);
-//        marker_n1.type(MarkerType.STAR4);
-//        marker_n1.color("#E80003");
-//
-//        List<DataEntry> marker2 = new ArrayList<>();
-//        marker2.add(new ValueDataEntry("正常", 0.3));
-//
-//        Marker marker_n2 = boxChart.marker(marker2);
-//        marker_n2.xPointPosition(0.5);
-//        marker_n2.legendItem().enabled(false);
-//        marker_n2.type(MarkerType.STAR4);
-//        marker_n2.color("#E80003");
-//
-//        List<DataEntry> marker3 = new ArrayList<>();
-//        marker3.add(new ValueDataEntry("正常", 0.6));
-//
-//        Marker marker_n3 = boxChart.marker(marker3);
-//        marker_n3.xPointPosition(0.65);
-//        marker_n3.legendItem().enabled(false);
-//        marker_n3.type(MarkerType.STAR4);
-//        marker_n3.color("#E80003");
-//
-//
-//        List<DataEntry> marker4 = new ArrayList<>();
-//        marker4.add(new ValueDataEntry("確診", 0.5));
-//
-//        Marker marker_n4 = boxChart.marker(marker4);
-//        marker_n4.xPointPosition(0.35);
-//        marker_n4.legendItem().enabled(false);
-//        marker_n4.type(MarkerType.STAR4);
-//        marker_n4.color("#E80003");
-//
-//        List<DataEntry> marker5 = new ArrayList<>();
-//        marker5.add(new ValueDataEntry("確診", 0.3));
-//
-//        Marker marker_n5 = boxChart.marker(marker5);
-//        marker_n5.xPointPosition(0.5);
-//        marker_n5.legendItem().enabled(false);
-//        marker_n5.type(MarkerType.STAR4);
-//        marker_n5.color("#E80003");
-//
-//        List<DataEntry> marker6 = new ArrayList<>();
-//        marker6.add(new ValueDataEntry("確診", 0.6));
-//
-//        Marker marker_n6 = boxChart.marker(marker6);
-//        marker_n6.xPointPosition(0.65);
-//        marker_n6.legendItem().enabled(false);
-//        marker_n6.type(MarkerType.STAR4);
-//        marker_n6.color("#E80003");
+        draw(boxChart);
+        markGene(boxChart);
 
 
         anyChartView.setChart(boxChart);
@@ -187,27 +64,56 @@ public class DNAmChartActivity extends AppCompatActivity {
         boxChart.credits().enabled(false);
         boxChart.legend().enabled(true);
 
-
     }
 
     private void draw(Cartesian boxChart) {
-//        List<DataEntry> data = new ArrayList<>();
-//        data.add(new CustomBoxDataEntry("正常", _lows.get(1), _q1.get(1), _q2.get(1), _q3.get(1), _highs.get(1), _outliers.get(1).toArray(new Double[0])));
+        String xName = "正常";
 
-        int size = 2;
-        for(int i = 0; i < 3; i++) {
+        int size = _q1.size();
+        for(int i = 0; i < size; i++) {
+            if(i < size/2)
+                xName = "正常";
+            else
+                xName = "罹癌";
+
             // OTX1_N
             List<DataEntry> data = new ArrayList<>();
-            data.add(new CustomBoxDataEntry("正常", _lows.get(i), _q1.get(i), _q2.get(i), _q3.get(i), _highs.get(i), _outliers.get(i).toArray(new Double[0])));
+            data.add(new CustomBoxDataEntry(xName, _lows.get(i), _q1.get(i), _q2.get(i), _q3.get(i), _highs.get(i), _outliers.get(i).toArray(new Double[0])));
 
             Box box = boxChart.box(data);
 //            box.fill(_colors[i]);
 //            box.stroke(_colors[i]);
-            box.stemStroke();
-            box.color(_colors[i]);
+//            box.stemStroke();
+//            box.medianStroke("#000000");
+            box.color(_colors.get(i));
             box.whiskerWidth("20%");
-            box.name(_name[i]);
-            box.xPointPosition(_xPos[i]);
+            box.name(_names.get(i));
+            box.xPointPosition(_xPos.get(i));
+
+            if(i >= size/2)
+                box.legendItem().enabled(false);
+
+        }
+    }
+
+    private void markGene(Cartesian boxChart) {
+        String xName;
+
+        int size = _q1.size();
+        for(int i = 0; i < size; i++) {
+            if(i < size/2)
+                xName = "正常";
+            else
+                xName = "罹癌";
+
+            List<DataEntry> data = new ArrayList<>();
+            data.add(new ValueDataEntry(xName, 0.5));
+
+            Marker marker = boxChart.marker(data);
+            marker.color("#E80003");
+            marker.type(MarkerType.STAR4);
+            marker.xPointPosition(_xPos.get(i));
+            marker.legendItem().enabled(false);
         }
     }
 
@@ -322,8 +228,36 @@ public class DNAmChartActivity extends AppCompatActivity {
                 }
                 _outliers.add(group);
             }
-            Log.d("5566", "dsfsdfsdfds");
             Log.d("5566", "outliers: " + _outliers.toString());
+
+            // ==================== _names ======================
+            String[] names = properties.getProperty("names", "").split(", ");
+
+            // Convert to ArrayList<String>
+            _names = new ArrayList<>(List.of(names));
+            _names.addAll(_names);
+            Log.d("5566", "_names: " + _names);
+
+            // ==================== _colors ======================
+            String[] colors = properties.getProperty("colors", "").split(", ");
+
+            // Convert to ArrayList<String>
+            _colors = new ArrayList<>(List.of(colors));
+            _colors.addAll(_colors);
+            Log.d("5566", "_colors: " + _colors);
+
+            // ==================== _lows ======================
+            String[] xPos = properties.getProperty("xPos", "").split(", ");
+
+            // Convert to ArrayList<Double>
+            _xPos = new ArrayList<>();
+            for (String value : xPos) {
+                if (value.isEmpty())
+                    throw new IllegalArgumentException("Empty value found in lows.");
+                _xPos.add(Double.parseDouble(value));
+            }
+            _xPos.addAll(_xPos);
+            Log.d("5566", "_xPos: " + _xPos);
         } catch (Exception e) {
             Log.d(_TAG, e.toString());
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show(); // Show Toast message
