797d99913f63fc667437b16a8b627656cb01c8c3
naitea9527 <a22057916w@gmail.com>
2024-03-01 00:39:15 +0800

    new second anyChartView

--- app/src/main/java/com/bilab/lunsenluandroid/main/setting/DNAmChartActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/DNAmChartActivity.java
@@ -8,14 +8,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.anychart.APIlib;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.SingleValueDataSet;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Cartesian;
+import com.anychart.charts.LinearGauge;
 import com.anychart.core.cartesian.series.Box;
 import com.anychart.core.cartesian.series.Marker;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.Layout;
 import com.anychart.enums.MarkerType;
+import com.anychart.enums.Orientation;
+import com.anychart.enums.Position;
+import com.anychart.scales.OrdinalColor;
 import com.bilab.lunsenluandroid.R;
 import com.bilab.lunsenluandroid.conf.Constant;
 
@@ -59,6 +67,7 @@ public class DNAmChartActivity extends AppCompatActivity {
         loadIntent();   // load personal gene value if exists
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        APIlib.getInstance().setActiveAnyChartView(anyChartView);
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
 
         Cartesian boxChart = AnyChart.box();
@@ -68,13 +77,82 @@ public class DNAmChartActivity extends AppCompatActivity {
 
         if(_hasGeneValue)
             markGene(boxChart);
-
-
         anyChartView.setChart(boxChart);
 
         // remove watermark "AnyChart Trial Version"
         boxChart.credits().enabled(false);
         boxChart.legend().enabled(true);
+
+        AnyChartView linearColorScale = findViewById(R.id.any_chart_liner_color_scale);
+        APIlib.getInstance().setActiveAnyChartView(linearColorScale);
+        LinearGauge linearGauge = AnyChart.linear();
+
+        linearGauge.data(new SingleValueDataSet(new Double[] { 5.3D }));
+
+        linearGauge.layout(Layout.HORIZONTAL);
+
+        linearGauge.label(0)
+                .position(Position.LEFT_CENTER)
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetY("-50px")
+                .offsetX("50px")
+                .fontColor("black")
+                .fontSize(17);
+        linearGauge.label(0).text("Total Rainfall");
+
+        linearGauge.label(1)
+                .position(Position.LEFT_CENTER)
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetY("40px")
+                .offsetX("50px")
+                .fontColor("#777777")
+                .fontSize(17);
+        linearGauge.label(1).text("Drought Hazard");
+
+        linearGauge.label(2)
+                .position(Position.RIGHT_CENTER)
+                .anchor(Anchor.RIGHT_CENTER)
+                .offsetY("40px")
+                .offsetX("50px")
+                .fontColor("#777777")
+                .fontSize(17);
+        linearGauge.label(2).text("Flood Hazard");
+
+        OrdinalColor scaleBarColorScale = OrdinalColor.instantiate();
+        scaleBarColorScale.ranges(new String[]{
+                "{ from: 0, to: 2, color: ['red 0.5'] }",
+                "{ from: 2, to: 3, color: ['yellow 0.5'] }",
+                "{ from: 3, to: 7, color: ['green 0.5'] }",
+                "{ from: 7, to: 8, color: ['yellow 0.5'] }",
+                "{ from: 8, to: 10, color: ['red 0.5'] }"
+        });
+
+        linearGauge.scaleBar(0)
+                .width("5%")
+                .colorScale(scaleBarColorScale);
+
+        linearGauge.marker(0)
+                .type(MarkerType.TRIANGLE_DOWN)
+                .color("red")
+                .offset("-3.5%")
+                .zIndex(10);
+
+        linearGauge.scale()
+                .minimum(0)
+                .maximum(10);
+//        linearGauge.scale().ticks
+
+        linearGauge.axis(0)
+                .minorTicks(false)
+                .width("1%");
+        linearGauge.axis(0)
+                .offset("-1.5%")
+                .orientation(Orientation.TOP)
+                .labels("top");
+
+        linearGauge.padding(0, 30, 0, 30);
+
+        linearColorScale.setChart(linearGauge);
 
     }
 
