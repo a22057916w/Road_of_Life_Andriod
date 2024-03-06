52d4dfc45ed8ba48f84a4972327d66570329d6cb
naitea9527 <a22057916w@gmail.com>
2024-03-07 00:37:30 +0800

    parse json from uri

--- app/src/main/java/com/bilab/lunsenluandroid/util/JsonUtils.java
+++ app/src/main/java/com/bilab/lunsenluandroid/util/JsonUtils.java
@@ -1,6 +1,9 @@
 package com.bilab.lunsenluandroid.util;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,6 +19,24 @@ public class JsonUtils {
     public static JSONObject readJsonFile(File jsonFile) throws IOException, JSONException {
         StringBuilder stringBuilder = new StringBuilder();
         try (InputStream inputStream = new FileInputStream(jsonFile);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return new JSONObject(stringBuilder.toString());
+    }
+
+    public static JSONObject readJsonFileFromUri(Context context, Uri uri) throws IOException, JSONException {
+        if (context == null) {
+            Log.d("5566", "Context is null");
+            throw new IllegalArgumentException("Context cannot be null");
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream = context.getContentResolver().openInputStream(uri);
              InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
