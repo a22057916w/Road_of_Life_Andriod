cf5b18464bfea127cd787f703ac9df58950f9562
naitea9527 <a22057916w@gmail.com>
2024-03-13 15:04:54 +0800

    add comments

--- app/src/main/java/com/bilab/lunsenluandroid/main/setting/HealthPassActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/HealthPassActivity.java
@@ -51,7 +51,8 @@ public class HealthPassActivity extends AppCompatActivity {
     private Button btn_read, btn_download;
     private TextView tv_demo;
 
-    // let user to select the file in the file system
+    // ActivityResultContract 會定義產生結果所需的輸入類型，以及結果的輸出類型。而 API 會針對拍照、要求取得權限等基本意圖動作提供預設合約
+    // ActivityResultContracts.GetContent(), let user select the file in the file system
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
             new ActivityResultCallback<Uri>() {
                 @Override
@@ -76,6 +77,8 @@ public class HealthPassActivity extends AppCompatActivity {
         btn_download = findViewById(R.id.btn_download);
 
         btn_read.setOnClickListener(new View.OnClickListener() {
+
+            // The input is the mime type to filter by, e.g. image/\*
             @Override
             public void onClick(View view) {
                 mGetContent.launch("application/json");
