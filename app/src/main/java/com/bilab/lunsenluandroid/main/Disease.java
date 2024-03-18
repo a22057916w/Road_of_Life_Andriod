807ec85de39349ba92828cb61780fa6f69de9727
naitea9527 <a22057916w@gmail.com>
2024-03-18 11:11:26 +0800

    add ICD methods

--- app/src/main/java/com/bilab/lunsenluandroid/main/Disease.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/Disease.java
@@ -1,12 +1,19 @@
 package com.bilab.lunsenluandroid.main;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+
 public class Disease {
     private final String _type;
     private final String _name;
 
+    private ArrayList<String> _icd;
+
     public Disease(String type, String name) {
         this._type = type;
         this._name = name;
+        loadICD();
     }
 
     public String getName() {
@@ -14,5 +21,12 @@ public class Disease {
     }
     public String getType() {
         return _type;
+    }
+    public ArrayList<String> getICD() { return _icd; }
+
+    public void loadICD() {
+        DiseaseData diseaseData = DiseaseData.getInstance();
+        _icd = diseaseData.getCancerICD10(_type, _name);
+//        Log.d("icd", _icd.toString());
     }
 }
