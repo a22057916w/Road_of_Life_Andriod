26ed5d48367f99ec4abbb2174beaa7ec3ce3f38e
naitea9527 <a22057916w@gmail.com>
2024-04-10 16:36:44 +0800

    add attr _icd9

--- app/src/main/java/com/bilab/lunsenluandroid/main/Disease.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/Disease.java
@@ -1,14 +1,13 @@
 package com.bilab.lunsenluandroid.main;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 
 public class Disease {
     private final String _type;
     private final String _name;
 
-    private ArrayList<String> _icd;
+    private Integer _icd9;
+    private ArrayList<String> _icd10;
 
     public Disease(String type, String name) {
         this._type = type;
@@ -22,11 +21,14 @@ public class Disease {
     public String getType() {
         return _type;
     }
-    public ArrayList<String> getICD() { return _icd; }
+    public Integer getICD9() { return _icd9; }
+    public ArrayList<String> getICD10() { return _icd10; }
 
     public void loadICD() {
         DiseaseData diseaseData = DiseaseData.getInstance();
-        _icd = diseaseData.getCancerICD10(_type, _name);
+        _icd10 = diseaseData.getCancerICD10(_type, _name);
+        _icd9 = diseaseData.getCancerICD9(_type, _name);
 //        Log.d("icd", _icd.toString());
     }
+
 }
