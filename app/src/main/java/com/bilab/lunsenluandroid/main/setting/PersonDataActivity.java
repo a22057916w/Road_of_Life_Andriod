4dd37511b877318e970c3700d734c4b9850cc8bc
naitea9527 <a22057916w@gmail.com>
2024-05-06 15:46:06 +0800

    new PersonDataActivity

---
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/PersonDataActivity.java
@@ -0,0 +1,16 @@
+package com.bilab.lunsenluandroid.main.setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bilab.lunsenluandroid.R;
+
+public class PersonDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_data);
+    }
+}
