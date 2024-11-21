d141ab537b8f8d389defad1307fbe63b4eda8e32
naitea9527 <a22057916w@gmail.com>
2024-11-21 20:34:22 +0800

    add CKD

--- app/src/main/java/com/bilab/lunsenluandroid/main/setting/HealthPassActivity.java
+++ app/src/main/java/com/bilab/lunsenluandroid/main/setting/HealthPassActivity.java
@@ -72,7 +72,7 @@ public class HealthPassActivity extends AppCompatActivity {
         Person person = Person.getInstance();
         DiseaseData diseasesData = DiseaseData.getInstance();
 
-        String[] cancers = {Constant.UTERUS, Constant.OVARY, Constant.BLADDER, Constant.RECTUM};
+        String[] cancers = {Constant.UTERUS, Constant.OVARY, Constant.BLADDER, Constant.RECTUM, Constant.CKD};
         String healthPass = jo_healthPass.toString();
 
         for (String cancer : cancers) {     // scan through each cancer
