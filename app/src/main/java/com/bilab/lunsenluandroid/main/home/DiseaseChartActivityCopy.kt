ebef15bef947c9bd655163821b984b10f4a7de1f
naitea9527 <a22057916w@gmail.com>
2025-10-24 18:35:31 +0800

    modify code

--- app/src/main/java/com/bilab/lunsenluandroid/main/home/DiseaseChartActivityCopy.kt
+++ app/src/main/java/com/bilab/lunsenluandroid/main/home/DiseaseChartActivityCopy.kt
@@ -57,16 +57,21 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
 
         setValue()
         loadConfig()
+
         registerUI()
+
         setUI()
         setHorizontalBarChart()
         setPieChart()
+
         computeRisk()
     }
 
     private fun registerUI() {
         tvCancer = findViewById(R.id.tv_cancer_diseases)
         tvOddsRatio = findViewById(R.id.tv_odds_ratio)
+
+        // charts
         horizontalBarChart = findViewById(R.id.horizontalBarChart)
         personalPieChart = findViewById(R.id.piechart_personal)
     }
@@ -76,6 +81,7 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
         cancerDiseases = DiseaseData.getInstance().getCancerDiseaseList(cancer)
         personDiseases = Person.getInstance().getDiseaseNames(cancer)
         cancerICD9s = DiseaseData.getInstance().getCancerICD9(cancer)
+
         pRisk = Person.getInstance().getRisk(cancer)
 
         Log.d("qwer", cancerDiseases.toString())
@@ -94,17 +100,19 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
         tvCancer.text = if (cancerName != Constant.NONE) "$cancerName - 共病風險分析" else Constant.NONE
     }
 
+    // include customized legends by RecyclerView
     private fun setHorizontalBarChart() {
         horizontalBarChart.apply {
-            // Set Y-Axes
+            // Set Left-Y Axis (圖形上方)
             axisLeft.setDrawGridLines(false)
+
+            // Set Right-Y Axis (圖形下方)
+            axisRight.setDrawGridLines(false)
+            axisRight.setDrawLabels(false)  // hide y-axis scales
+
+            // Ensure Y-axis starts from 0
             axisLeft.axisMinimum = 0f
-            axisRight.apply {
-                setDrawGridLines(false)
-                setDrawLabels(false)
-                axisMaxValue = 16f
-                axisMinValue = 0f
-            }
+            axisRight.axisMinimum = 0f
 
             // Set X Axis
             xAxis.apply {
@@ -117,51 +125,54 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
             }
 
             // Set bar values
-            val dataSets = mutableListOf<BarDataSet>()
+            val dataSets = mutableListOf<BarDataSet>()      // one bar in one data-set
             val diseaseData = DiseaseData.getInstance()
-            val barCount = (cancerDiseases.size - 1) * 2
+            val barCount = (cancerDiseases.size - 1) * 2    // 減去 "無上述症狀"
 
             for (i in 0 until barCount) {
-                val entries = mutableListOf<BarEntry>()
+                val entries = mutableListOf<BarEntry>()     // the size is always one(bar)
                 val pos = barCount - i
                 val color: Int
-                val label = cancerDiseases[i / 2]
 
+                // the title bar config
                 if (i % 2 == 0) {
                     entries.add(BarEntry(pos.toFloat(), 0.0f))
                     color = Color.LTGRAY
-                } else {
-                    if (personDiseases.contains(label)) {
+                }
+                // the data bar config
+                else {
+                    if (personDiseases.contains(cancerDiseases[i / 2])) {
                         entries.add(BarEntry(pos.toFloat(), diseaseData.getICD9OR(cancer, cancerICD9s[i / 2]).toFloat()))
                         color = Color.parseColor("#FF3333") // RED
-                    } else {
+                    }
+                    else {
                         entries.add(BarEntry(pos.toFloat(), 0.15f))
                         color = Color.GRAY
                     }
                 }
 
-                val barDataSet = BarDataSet(entries, label).apply {
+                val barDataSet = BarDataSet(entries, cancerDiseases[i / 2]).apply {
                     this.color = color
-                    setDrawValues(false)
+                    setDrawValues(false)    // disable showing value at the end of the bar
                     axisDependency = YAxis.AxisDependency.RIGHT
                 }
-                dataSets.add(barDataSet)
+                dataSets.add(barDataSet)    // add a dataset in which only one bar exists
             }
 
             // Set bar UI
-            val barData = BarData(dataSets.toList()).apply {
-                barWidth = 1.0f
-            }
+            val barData = BarData(dataSets.toList())
+            barData.barWidth = 1.0f
 
+            // 只顯示 title bar 的 value(label)
             xAxis.valueFormatter = object : ValueFormatter() {
                 private val size = cancerDiseases.size - 1
                 override fun getFormattedValue(value: Float): String {
                     return if (value.toInt() % 2 == 0) cancerDiseases[size - value.toInt() / 2] else ""
                 }
             }
-            xAxis.labelCount = dataSets.size
+            xAxis.labelCount = dataSets.size    // 設定 X 軸標籤的顯示數量
 
-            // Final chart settings
+            // 設定其他屬性
             setDrawValueAboveBar(true)
             description.isEnabled = false
             setDrawGridBackground(false)
@@ -170,7 +181,7 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
             isClickable = false
             setDrawBorders(true)
             setBorderColor(Color.GRAY)
-            borderWidth = 0.2f
+//            borderWidth = 0.2f
             setTouchEnabled(false)
 
             // Set data
@@ -187,6 +198,7 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
         val personalPieDataSet = PieDataSet(entries, "")
         val personalPieData = PieData(personalPieDataSet)
 
+        // set common attribute
         val text = when {
             pRisk <= 50.0 -> "低風險"
             pRisk <= 75.0 -> "中風險"
@@ -194,6 +206,7 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
         }
         initPieChart(personalPieChart, personalPieData, text)
 
+        // set pie colors
         val chartRatio = ceil((cancerDiseases.size - 1) * pRisk / 100).toInt()
         val color = when {
             pRisk <= 50.0 -> Color.GREEN
@@ -202,22 +215,24 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
         }
         setPieColor(personalPieData, chartRatio, color)
 
+        // draw charts
         personalPieChart.data = personalPieData
     }
 
     private fun initPieChart(pieChart: PieChart, pieData: PieData, text: String) {
         pieData.apply {
-            setValueFormatter(PercentFormatter(pieChart))
+            setValueFormatter(PercentFormatter(pieChart))   // Format values as percentages
             setDrawValues(false)
         }
 
+        // Customize the chart
         pieChart.apply {
             description.isEnabled = false
             holeRadius = 90f
             transparentCircleRadius = 45f
             legend.isEnabled = false
             centerText = text
-            centerTextSize = 16f
+//            centerTextSize = 16f
             rotationAngle = 0f
             animateY(1000, Easing.EaseInOutQuad)
             invalidate()
@@ -250,8 +265,10 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
     }
 
     private fun loadConfig() {
+        // Load properties from assets
         val properties = Properties()
         val assetManager: AssetManager = assets
+
         try {
             val inputStream: InputStream = assetManager.open("disease_chart_activity.properties")
             properties.load(inputStream)
@@ -259,24 +276,30 @@ class DiseaseChartActivityCopy : AppCompatActivity() {
             e.printStackTrace()
         }
 
-        val cancerLower = cancer.toLowerCase()
-
+        val cancerLower = cancer.lowercase()
         try {
-            val weightsKey = "$cancerLower.model.weights"
+            // ===================== weights ========================
+            val weightsKey = "$cancerLower.model.weights"   // e.g. bladder.model.weights
             val wDiseasesStr = properties.getProperty(weightsKey, "").replace(Regex("[()]"), "").split(",\s*".toRegex())
 
+            // Iterate over tuple array and populate the map
             wDiseases = if (wDiseasesStr.isNotEmpty() && wDiseasesStr[0].isNotEmpty()) {
                 wDiseasesStr.toList().chunked(2).associate { (key, value) -> key to value.toDouble() }
             } else {
                 emptyMap()
             }
 
+            // Print the map
             wDiseases.forEach { (key, value) -> Log.d("qwer", "$key -> $value") }
 
+            // ====================== bias ==============================
             val modelBiasKey = "$cancerLower.model.bias"
             val biasStr = properties.getProperty(modelBiasKey, "")
+
+            // convert to Double
             if (biasStr.isEmpty()) throw IllegalArgumentException("Empty value found in bias.")
             bias = biasStr.toDouble()
+
             Log.d("qwer", "bias: $bias")
 
         } catch (e: Exception) {
